1ac2453dce92053a7c3c5c49694881a4f2e26339
Brenden Matthews <brenden@brndn.io>
2020-08-06 00:08:09 -0400

    Bump version.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Citrine.MixProject do
   def project do
     [
       app: :citrine,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
